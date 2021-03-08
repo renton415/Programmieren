e9960e8f8f00cce4bc482b8b55993b0aa362e5d7
Alexander <snowystatic@web.de>
2021-03-08 16:33:24 +0100

    added exponentiation

--- Programmieren/src/de/dhbwka/java/exercise/methods/Exponentiation.java
+++ Programmieren/src/de/dhbwka/java/exercise/methods/Exponentiation.java
@@ -18,14 +18,30 @@ public class Exponentiation {
 	public void setExp(int exp) {
 		this.exp = exp;
 	}
+	
+	static double exp(double x, int n)
+	{
+		if (n == 0)
+		{
+			return 1.0;
+		}
+		else {
+			return x * exp(x, n-1);
+		}
+	}
 
 
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Geben Sie bitte die Basis ein: ");
-		 
-
+		double base = scan.nextDouble();
+		System.out.println("Geben Sie bitte den positiven ganzzahligen Exponenten ein: ");
+		int exp = scan.nextInt();
+		
+		System.out.println(base+"^ "+exp+" =  " + exp(base,exp));
+		
+		scan.close();
 	}
 
 }
