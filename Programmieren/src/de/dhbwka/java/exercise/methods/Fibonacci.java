3afbb5e80d9b98863cffde48ce1f5465a6990a47
Alexander <snowystatic@web.de>
2021-03-12 10:09:38 +0100

    fibancci recursive implementation added quicksort class

--- Programmieren/src/de/dhbwka/java/exercise/methods/Fibonacci.java
+++ Programmieren/src/de/dhbwka/java/exercise/methods/Fibonacci.java
@@ -1,10 +1,26 @@
 package de.dhbwka.java.exercise.methods;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static long fibonacci(long n) { 
+        if (n <= 2) { 
+            return 1; 
+        } 
+        return fibonacci(n - 1) + fibonacci(n - 2); 
+    } 
 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Geben Sie bitte die Basis ein: ");
+		int n = scan.nextInt();
+		for (int i = 0; i<n; i++) {
+			System.out.println("F( "+i+") =   "+fibonacci(i));
+		}
+
+
+		scan.close();
 	}
 
 }
---
+++ Programmieren/src/de/dhbwka/java/exercise/methods/Quicksort.java
@@ -0,0 +1,7 @@
+package de.dhbwka.java.exercise.methods;
+
+public class Quicksort {
+
+    public static void main(String[] args) {
+    }
+}
