00d1be1ae7f1005e82302d1978824d961e56b31e
Alexander <snowystatic@web.de>
2021-04-07 09:12:03 +0200

    added Thread

--- Programmieren/src/de/dhbwka/java/exercise/threads/DancingText.java
+++ Programmieren/src/de/dhbwka/java/exercise/threads/DancingText.java
@@ -2,6 +2,8 @@ package de.dhbwka.java.exercise.threads;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import java.awt.Graphics;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class DancingText extends JFrame implements Runnable{
@@ -11,7 +13,7 @@ public class DancingText extends JFrame implements Runnable{
 
     public DancingText() {
         super("Dancing Text");
-        this.add(this.dancingTextField);
+        //this.add(this.dancingTextField);
         this.runner = new Thread(this);
         this.runner.start();
 
@@ -19,11 +21,18 @@ public class DancingText extends JFrame implements Runnable{
     public void run () {
         while(true) {
             try {
-
+                Thread.sleep(500);
             } catch (InterruptedException ex) {
 
             }
         }
+    }
+
+    @Override 
+    public void paint(Graphics g) {
+        g.setFont(new Font("Helvetica", Font.BOLD, 40)); 
+        g.drawString("D", 40, 75);
+        g.drawString("n", 10, 75);
     }
 
     public static void main (String[] args){
